893b1fe182b924ab201830f34a0c2d624eb0a00d
Clay Ellis <clayellis@me.com>
2018-07-26 09:23:07 -0600

    Update to distinguish between updating the current and initial environment variables

--- Sources/Postman/Models/PostmanClient.swift
+++ Sources/Postman/Models/PostmanClient.swift
@@ -1,5 +1,6 @@
 import Vapor
 
+/// A client which can make calls to the Postman API.
 public final class PostmanClient: Service {
     let httpClient: Client
     let apiKey: String
@@ -31,6 +32,12 @@ public final class PostmanClient: Service {
         let error: PostmanError
     }
 
+    /// Gets the environment.
+    ///
+    /// - note: The values are the *initial* environment variables, not the current.
+    ///
+    /// - returns: A future `PostmanEnvironment`.
+    ///
     public func getEnvironment() -> Future<PostmanEnvironment> {
 
         let request = httpClient.get(environmentEndpoint, headers: headers)
@@ -48,10 +55,13 @@ public final class PostmanClient: Service {
         }
     }
 
-    public func update(_ environment: PostmanEnvironment) -> Future<Void> {
+    /// Updates your environment by replacing the *initial* environment variables with those from the new environment.
+    ///
+    /// - parameter newEnvironment: The environment whose `name` and `values` will be used to update the initial environment variables.
+    ///
+    public func updateInitialEnvironment(byReplacingWith newEnvironment: PostmanEnvironment) -> Future<Void> {
 
-        let parameters = EnvironmentContainer(environment: environment)
-
+        let parameters = EnvironmentContainer(environment: newEnvironment)
         let request = httpClient.put(environmentEndpoint, headers: headers, beforeSend: { request in
             try request.content.encode(parameters)
         })
@@ -66,4 +76,56 @@ public final class PostmanClient: Service {
             }
             }.transform(to: ())
     }
+
+    /// Updates your environment by getting your current environment, merging it with the other environment using the provided strategy,
+    /// and then replacing the *initial* environment variables with those from the merged environment.
+    ///
+    /// - parameter otherEnvironment: The environment which will be merged into the current environment.
+    /// - parameter strategy: See `PostmanEnvironment.MergeStrategy`.
+    ///
+    public func updateInitialEnvironment(byMergingWith otherEnvironment: PostmanEnvironment, strategy: PostmanEnvironment.MergeStrategy) -> Future<Void> {
+        return getEnvironment().flatMap { currentEnvironment in
+            let mergedEnvironment = currentEnvironment.mergingValues(from: otherEnvironment, strategy: strategy)
+            return self.updateInitialEnvironment(byReplacingWith: mergedEnvironment)
+        }
+    }
+
+    /// Updates your environment by replacing the *current* environment variables with those from the new environment.
+    ///
+    /// - note: Your current environment variables are updated by first deleting the entire environment and then replacing it.
+    ///         This is a workaround in the way that Postman's API works.
+    ///
+    /// - parameter newEnvironment: The environment whose `name` and `values` will be used to update the current environment variables.
+    ///
+    public func updateCurrentEnvironment(byReplacingWith newEnvironment: PostmanEnvironment) -> Future<Void> {
+        var emptyEnvironment = newEnvironment
+        emptyEnvironment.values = [:]
+        return updateInitialEnvironment(byReplacingWith: emptyEnvironment).flatMap { _ in
+            self.updateInitialEnvironment(byReplacingWith: newEnvironment)
+        }
+    }
+
+    /// Updates your environment by getting your current environment, merging it with the other environment using the provided strategy,
+    /// and then replacing the *current* environment variables with those from the merged environment.
+    ///
+    /// - note: Your current environment variables are updated by first deleting the entire environment and then replacing it.
+    ///         This is a workaround in the way that Postman's API works.
+    ///
+    /// - parameter otherEnvironment: The environment which will be merged into the current environment.
+    /// - parameter strategy: See `PostmanEnvironment.MergeStrategy`.
+    ///
+    public func updateCurrentEnvironment(byMergingWith otherEnvironment: PostmanEnvironment, strategy: PostmanEnvironment.MergeStrategy) -> Future<Void> {
+        return getEnvironment().flatMap { currentEnvironment in
+            let mergedEnvironment = currentEnvironment.mergingValues(from: otherEnvironment, strategy: strategy)
+            return self.updateCurrentEnvironment(byReplacingWith: mergedEnvironment)
+        }
+    }
 }
+
+// TODO: Deprecate this
+//extension PostmanClient {
+//
+//    public func update(_ environment: PostmanEnvironment) -> Future<Void> {
+//
+//    }
+//}
